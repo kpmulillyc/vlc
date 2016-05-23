b01d2e2dfc307217071b66108776a095f29dc128
kpmulillyc <kpmulillyc@gmail.com>
2016-05-23 12:22:02 +0800

    first beautiful version

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import json, subprocess,urllib.request,urllib.request,urllib.parse
 from flask import Flask, render_template,request, jsonify
 import urllib.request
+from subprocess import PIPE
 
 
 
-def apiCall(category):
-    req = urllib.request.Request('https://api.twitch.tv/kraken/'+category)
+def apiCall(entries):
+    req = urllib.request.Request('https://api.twitch.tv/kraken/games/top?limit=5&offset='+str(entries))
     url = urllib.request.urlopen(req).read().decode("utf-8")
     return json.loads(url)
 
-def apiGame(games):
-    req = urllib.request.Request('https://api.twitch.tv/kraken/streams?game=' + games)
+def apiGame(games,entries):
+    req = urllib.request.Request('https://api.twitch.tv/kraken/streams?game=' + games+"&limit=5&offset="+str(entries))
     url = urllib.request.urlopen(req).read().decode("utf-8")
     return json.loads(url)
 
@@ -19,16 +20,29 @@ def apiChannel(channel):
     url = urllib.request.urlopen(req).read().decode("utf-8")
     return json.loads(url)
 
+def search(query):
+    req = urllib.request.Request('https://api.twitch.tv/kraken/search/channels?q=' + query)
+    url = urllib.request.urlopen(req).read().decode("utf-8")
+    return json.loads(url)
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template("index.html", call = apiCall('games/top?limit=5')['top'])
+@app.route('/<int:id>')
+def main(id=0):
+    qq = id
+    if id > 0:
+        qq = id * 5
+    return render_template("index.html", call = apiCall(qq)['top'],id=id)
 
 @app.route('/game/<games>')
-def game(games):
+@app.route('/game/<games>/<int:id>')
+def game(games,id=0):
+    qq = id
+    if id > 0:
+        qq = id * 5
     games = urllib.parse.quote(games)
-    return render_template("games.html", call = apiGame(games)['streams'])
+    return render_template("games.html", call = apiGame(games,qq)['streams'],id=id,games=games)
 
 @app.route('/player/<channel>')
 def player(channel):
@@ -54,14 +68,17 @@ def stop():
     call2.terminate()
     call2.wait()
 
-@app.route('/check', methods=['POST'])
+@app.route('/check')
 def check():
-    #cmd = ['nc -vn 127.0.0.1 8913']
-    #p = subprocess.check_output(cmd,shell=True)
-    return jsonify('test')
-    #p.terminate()
-    #p.wait()
-
+    cmd = ["nc -vn 127.0.0.1 8913"]
+    p = subprocess.Popen(cmd,shell=True,stdin=PIPE,stdout=PIPE,stderr=PIPE)
+    output, error = p.communicate()
+    if p.returncode==0:
+        return jsonify(result="Server is running")
+    else:
+        return jsonify(result="Server is DOWN")
+    p.terminate()
+    p.wait()
 
 if __name__ == '__main__':
     app.run()
